d193dde602648c529c76479fe828606fca7b704f
FishyMaroc <ilyasmouhssine1998@hotmail.com>
2026-03-28 11:06:09 +0000

    Added a cool bar chart of the top movies

--- 2_database.py
+++ 2_database.py
@@ -1,5 +1,7 @@
 import sqlite3
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 # 1. Load the data we scraped in Step 2
 df = pd.read_csv("movies.csv")
@@ -12,3 +14,23 @@ df.to_sql("rt_ratings", connection, if_exists="replace", index=False)
 
 print("SQL Database 'movie_data.db' created and populated!")
 connection.close()
+
+# Load the data
+df = pd.read_csv('movies.csv')
+
+# Clean the scores (remove % and make them numbers)
+df['Critic_Score'] = pd.to_numeric(df['Critic_Score'].str.replace('%', ''), errors='coerce')
+
+# Sort by top 10 movies
+top_10 = df.sort_values(by='Critic_Score', ascending=False).head(10)
+
+# Create the Chart
+plt.figure(figsize=(10, 6))
+plt.barh(top_10['Title'], top_10['Critic_Score'], color='tomato')
+plt.xlabel('Critic Score (%)')
+plt.title('Top 10 Netflix Movies on Rotten Tomatoes')
+plt.gca().invert_yaxis() # Put the #1 movie at the top
+
+# Save the picture!
+plt.savefig('movie_chart.png')
+print("🎨 Chart saved as movie_chart.png!")
